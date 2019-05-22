740102dddf27611c320676b624b55299c3b45a84
webeyez <webeye@live.com>
2019-05-22 02:04:01 -0700

    Updated L2_shift.sh

--- L2_shift.sh
+++ L2_shift.sh
@@ -8,5 +8,13 @@ shift
 echo "The arguments provided now are $*"
 echo "The new value of \$1 is $1"
 
+# Below code is to show the use of {}, this will print the 10th arg. Please note that becasue of shift first arg has been removed 
+# hence enter 11 arg while running the program
+
+if [ $# -eq 10 ]
+then
+	echo "The last argumnet is ${10}"	
+fi
+
 exit
 
